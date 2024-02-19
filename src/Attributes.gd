2fe4e7c9d17342e2bc2ddde3b3f036df99fc060d
edwardkelley <kelle291@purdue.edu>
2024-02-19 03:13:32 -0500

    Added Basic Main Menu and Skeletons for Save/Load Game Save Scripts

---
+++ src/Attributes.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var xp = 0
+var xpos = 0
+var ypos = 0
+var hasCollectible = 0
--- src/SaveUtils.gd
+++ src/SaveUtils.gd
@@ -1,15 +1,17 @@
 extends Node
 
-class_name SaveUtils
-
 const SAVE_FILENAME = "res://save.bin"
 # To be implemented later when player is made
 func save():
 	var file = FileAccess.open(SAVE_FILENAME, FileAccess.WRITE)
-	#var save_data: Dictionary {
-	#	"xposition" : player.xpos
-	#	"yposition" : player.ypos
-	#}
+	var save_data: Dictionary = {
+		"xp" : Attributes.xp,
+		"xposition" : Attributes.xpos,
+		"yposition" : Attributes.ypos,
+		"hasCollectible" : Attributes.hasCollectible
+	}
+	var string_data = JSON.stringify(save_data)
+	file.store_line(string_data)
 	file.close()
 
 # To be implemented later when player is made
