6c1ced51a5cd21f7e58264f4b8e862bd10f2fcfa
flynn86 <colinflynn514@gmail.com>
2024-02-21 14:42:26 -0500

    updating location tab connection

--- src/main_menu.gd
+++ src/main_menu.gd
@@ -4,13 +4,13 @@ func _on_start_new_game_pressed():
 	SaveUtils.initialize_game()
 	
 	# Placeholder for changing to Map scene
-	get_tree().change_scene_to_file("res://scenes/location_page.tscn")
+	get_tree().change_scene_to_file("res://scenes/location_tab/location_page.tscn")
 
 
 func _on_load_game_pressed():
 	SaveUtils.load()
 	
 	# Placeholder for changing to Map scene
-	get_tree().change_scene_to_file("res://scenes/location_page.tscn")
+	get_tree().change_scene_to_file("res://scenes/location_tab/location_page.tscn")
 
 
