7ba67673f7f82e458040e9b6d6a1211116fa1586
flynn86 <colinflynn514@gmail.com>
2024-02-23 14:44:38 -0500

    Updating layout for club page, adding info for four groups of clubs, adding search bar and buttons

--- scenes/club_page/club_page.gd
+++ scenes/club_page/club_page.gd
@@ -1,34 +1,84 @@
 extends Panel
 
 var items = {}
-# Called when the node enters the scene tree for the first time.
+var popup
+
 func _ready():
 	#OS.shell_open("https://boilerlink.purdue.edu/events")
-	_get_club_info()
+	_get_club_info("sport_info")
+	_get_club_info("frat_info")
+	_get_club_info("sor_info")
+	_get_club_info("other_info")
+	$sport_bar.connect("text_changed", self._on_sport_text_changed)
+	$other_bar.connect("text_changed", self._on_academic_text_changed)
+	$sor_bar.connect("text_changed", self._on_sor_text_changed)
+	$frat_bar.connect("text_changed", self._on_frat_text_changed)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
-
-func _get_club_info():
-	var text_path = "res://scenes/club_page/club_info.txt"
+func _get_club_info(file_name: String):
+	var text_path = "res://scenes/club_page/%s.txt" % file_name
 	if FileAccess.file_exists(text_path):
 		var file = FileAccess.open(text_path, FileAccess.READ)
 		var text_content = file.get_as_text()
 		var lines = text_content.split("\n")
 		for line in lines:
-			var split_line = line.split(":")  # Assuming the format is 'name:description'
+			var split_line = line.split(":")
 			if split_line.size() >= 2:
 				var name = split_line[0].strip_edges()
 				var description = split_line[1].strip_edges()
 				items[name] = description
+		if file_name == "sport_info":
+			_generate_buttons("sport")
+			items = {}
+		elif file_name == "other_info":
+			_generate_buttons("academic")
+			items = {}
+		elif file_name == "sor_info":
+			_generate_buttons("sor")
+			items = {}
+		else:
+			_generate_buttons("frat")
+			items = {}
 
-func _clean_items():
-	var cleaned_items = {}
-	for key in items:
-		print(key)
-		var cleaned_key = key.replace("\\", "")
-		print(cleaned_key)
-		var cleaned_value = items[key].replace("\\", "")
-		cleaned_items[cleaned_key] = cleaned_value
+func _generate_buttons(cont_name: String):
+	var container = get_node("%s_scroll/%s_container" % [cont_name, cont_name]) as VBoxContainer
+	
+	for name in items.keys():
+		var button = Button.new()
+		button.text = name
+		button.connect("pressed", self._button_pressed)
+		container.add_child(button)
+
+func _button_pressed():
+	print("hello")
+
+func _on_academic_text_changed(new_text):
+	var container = get_node("academic_scroll/academic_container") as VBoxContainer
+	for button in container.get_children():
+		if  new_text.is_empty() or new_text.to_lower() in button.text.to_lower():
+			button.show()
+		else:
+			button.hide()
+
+func _on_sport_text_changed(new_text):
+	var container = get_node("sport_scroll/sport_container") as VBoxContainer
+	for button in container.get_children():
+		if new_text.is_empty() or new_text.to_lower() in button.text.to_lower():
+			button.show()
+		else:
+			button.hide()
+
+func _on_sor_text_changed(new_text):
+	var container = get_node("sor_scroll/sor_container") as VBoxContainer
+	for button in container.get_children():
+		if  new_text.is_empty() or new_text.to_lower() in button.text.to_lower():
+			button.show()
+		else:
+			button.hide()
+
+func _on_frat_text_changed(new_text):
+	var container = get_node("frat_scroll/frat_container") as VBoxContainer
+	for button in container.get_children():
+		if  new_text.is_empty() or new_text.to_lower() in button.text.to_lower():
+			button.show()
+		else:
+			button.hide()
