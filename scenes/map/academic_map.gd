f417d557c9fcac4da7fcd21e1c3d3424355cefca
flynn86 <colinflynn514@gmail.com>
2024-02-29 12:55:40 -0500

    implementing returning to the same location after tab is closed

--- scenes/map/academic_map.gd
+++ scenes/map/academic_map.gd
@@ -2,12 +2,20 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var text_path1 = "res://scenes/map/char_pos.txt"
+	if FileAccess.file_exists(text_path1):
+		var file = FileAccess.open(text_path1, FileAccess.READ)
+		var position_string = file.get_line()
+		var position_xy = position_string.split(",")
+		var x = float(position_xy[0])
+		var y = float(position_xy[1])
+		var position = Vector2(x, y)
+		$CharacterBody2D.global_position = position
 	set_process_input(true)
 	
 
 func _input(event):
 	if event is InputEventMouseMotion or event is InputEventMouseButton:
-		# Convert the event's global position to the tilemap's local space.
 		var mouse_pos = event.position
 
 func _on_Area2D_input_event(viewport, event, shape_idx, build_name: String):
@@ -20,7 +28,8 @@ func open_tab(building_name: String):
 	var text_path1 = "res://scenes/map/char_pos.txt"
 	if FileAccess.file_exists(text_path1):
 		var file = FileAccess.open(text_path1, FileAccess.WRITE)
-		#file.store_string($CharacterBody2D.global_position)
+		var position_string = str($CharacterBody2D.global_position.x) + ", " + str($CharacterBody2D.global_position.y)
+		file.store_string(position_string)
 		file.close()
 	if building_name == "PMU":
 		get_tree().change_scene_to_file("res://scenes/PMU_page/pmu_panel.tscn")
