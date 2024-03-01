6b9cf176b6dd7f24352668896544a1eb8a1323f3
Charlie Zhang <zhan4538@purdue.edu>
2024-02-29 23:14:12 -0600

    Day/Night button

--- scenes/map/day_night.gd
+++ scenes/map/day_night.gd
@@ -1,12 +1,10 @@
 extends CanvasModulate
 
-var gt = preload("res://GlobalTime.gd").new()
+var state = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var time_of_day = fmod(gt.global_time, 24.0)
-
-	if (time_of_day >= 6.0 and time_of_day < 18.0):
+	if (state == 0):
 		visible = false
 	else:
 		visible = true
