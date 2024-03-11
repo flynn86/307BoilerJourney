fd69650766c7aba47ff10ab8e117b29cdfb8e54d
edwardkelley <kelle291@purdue.edu>
2024-03-10 23:46:23 -0400

    Implemented Client-Server UI. Began working on Clinet-Server connections. Made minor adjustments to username selection UI.

--- characters/character_built.gd
+++ characters/character_built.gd
@@ -1,18 +1,17 @@
 extends Node2D
 
+@onready var username_entry = $LineEdit
+
 func _on_button_pressed():
-	if Attributes.username == "":
+	if username_entry.text == "":
 		$Label.visible = true
 	else:
+		Attributes.username = username_entry.text
 		get_tree().change_scene_to_file("res://characters/select_avatar.tscn")
 
-
 func _on_button_2_pressed():
-	if Attributes.username == "":
+	if username_entry.text == "":
 		$Label.visible = true
 	else:
+		Attributes.username = username_entry.text
 		get_tree().change_scene_to_file("res://characters/Player.tscn")
-
-
-func _on_line_edit_text_changed(new_text):
-	Attributes.username = new_text
--- scenes/menu.gd
+++ scenes/menu.gd
@@ -11,3 +11,39 @@ func _on_back_to_main_menu_button_pressed():
 
 func _on_customize_character_button_pressed():
 	get_tree().change_scene_to_file("res://characters/Player.tscn")
+
+const PORT = 5678
+@onready var address_entry = $LineEdit
+var enet = ENetMultiplayerPeer.new()
+
+func _on_join_server_button_pressed():
+	if address_entry.text == "":
+		$Label2.visible = true
+	else:
+		$Label2.visible = false
+		$HostServerButton.visible = false
+		$JoinServerButton.visible = false
+		$LineEdit.visible = false
+		multiplayer.multiplayer_peer = enet
+		enet.create_client(address_entry.text, PORT)
+
+func _on_host_server_button_pressed():
+	$Label2.visible = false
+	$HostServerButton.visible = false
+	$JoinServerButton.visible = false
+	$LineEdit.visible = false
+	enet.create_server(PORT)
+	multiplayer.multiplayer_peer = enet
+	multiplayer.peer_connected.connect(add_client)
+	multiplayer.peer_disconnected.connect(remove_client)
+	add_client(multiplayer.get_unique_id())
+	setup_upnp()
+
+func add_client(id):
+	pass # Need to Implement
+	
+func remove_client(id):
+	pass # Need to Implement
+	
+func setup_upnp():
+	pass # Need to Implement
