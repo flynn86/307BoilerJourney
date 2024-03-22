4d646eb6ded411c74d1e6100b28a0a972dc12e25
Shubhaang2503 <agarw308@purdue.edu>
2024-03-22 19:39:09 -0400

    Implemented view scehdule

--- Schedule.gd
+++ Schedule.gd
@@ -3,7 +3,29 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var complete_string = ""
+	if (Attributes.course_num == 1) :
+		complete_string = "No Courses Added yet. Please add a course first"
+	else:
+		for i in range (1, Attributes.course_num):
+
+			var name = "course" + str(i) + "_name"
+			var courseName = Attributes.courseNames[name]
+			var time = "course" + str(i) + "_time"
+			var courseTime = Attributes.courseTimes[time]
+			var location = "course" + str(i) + "_location"
+			var courseLocation = Attributes.courseLocations[location]
+			var day = "course" + str(i) + "_days"
+			var Days_dict = Attributes.courseDays[day]
+			var courseDays = ""
+			for j in Days_dict:
+				if Days_dict[j] == 1:
+					courseDays += j
+			var format_string = "| Course Name: %s | Time: %s | Days: %s | Location: %s |\n"
+			var actual_string = format_string % [courseName, courseTime, courseDays, courseLocation]
+			complete_string += actual_string
+	$Label.text = complete_string
+			
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +35,6 @@ func _process(delta):
 
 func _on_add_pressed():
 	get_tree().change_scene_to_file("res://scenes/addschedule.tscn")
+
+
+
