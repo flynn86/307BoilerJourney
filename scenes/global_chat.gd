d7f2ddfc081c4fead2a8e005fb99c76b8ce7919f
edwardkelley <kelle291@purdue.edu>
2024-03-24 23:39:27 -0400

    Formatted Global Chat Message View

--- scenes/global_chat.gd
+++ scenes/global_chat.gd
@@ -2,14 +2,19 @@ extends Node2D
 
 
 func _ready():
-	$Label.text = str((Attributes.database).select_rows("GlobalChat", "username != 'null'", ["*"]))
-	print((Attributes.database).select_rows("GlobalChat", "username != 'null'", ["*"]))
+	var data : Array = (Attributes.database).select_rows("GlobalChat", "username != 'null'", ["*"])
+	var data_line : String = ""
+	for i in data.size():
+		data_line += "From " + data[i].username + " at " + data[i].time + " on " + data[i].date + ": " + data[i].message + '\n'
+	$Label.text = data_line
 
 func _on_send_message_pressed():
+	var timedate : Dictionary = Time.get_datetime_dict_from_system()
 	var data = {
 		"username" = Attributes.username,
 		"message" = $LineEdit.text,
-		"time_date" = Time.get_datetime_string_from_system()
+		"date" = str(timedate.month) + "-" + str(timedate.day) + "-" + str(timedate.year),
+		"time" = str(timedate.hour) + ":" + str(timedate.minute) + ":" + str(timedate.second)
 	}
 	(Attributes.database).insert_row("GlobalChat", data)
 	_ready()
