9abb2a5c60e5249be55fce08657923dcca2f7c58
Charlie Zhang <zhan4538@purdue.edu>
2024-03-26 20:35:44 -0500

    Editing inventory scripts

--- scenes/Inventory/Inventory_slot.gd
+++ scenes/Inventory/Inventory_slot.gd
@@ -1,20 +1,19 @@
-class_name Inventory_Slot extends TextureRect
+extends TextureRect
 
 var item
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if item:
-		%item_container.add_child(item)
+		$item_container.add_child(item)
 
 func set_item(new_item):
 	item = new_item
 	
 func pick_item():
-	%item_container.remove_child(item)
+	$item_container.remove_child(item)
 	item = null
 
 func put_item(new_item):
 	item = new_item
-	#item.put_item()
-	%item_container.add_child(item)
+	$item_container.add_child(item)
