f27b506184c965e8424a2036fd617ed2cec3f645
Shubhaang2503 <agarw308@purdue.edu>
2024-03-26 22:55:06 -0400

    Show courses at that building implemented

--- scenes/map/academic_map.gd
+++ scenes/map/academic_map.gd
@@ -25,6 +25,11 @@ func _ready():
 	get_node("Purdue_symbol").visible = !Attributes.purdue_symbol
 	get_node("IU_Sucks_Poster").visible = !Attributes.iu_poster
 	get_node("basketball").visible = !Attributes.basketball
+	var building_list = ["ARMS", "PUSH", "HAMP", "PHYS", "RNPH", "JSN", "FRNY", "DSCB", "MSEE", "CHAS", "BHEE", "AR", "ELLT", 
+					"HOVD", "ME", "WALC", "POTR", "LMBS", "KNOY", "DUDL", "HAAS", "PSYC", "PRCE", "CL50", "MATH", "SC", 
+					"WTHR", "BRWN", "HEAV", "GRIS", "BRNG", "SCHM", "UNIV", "MTHW", "STON", "STEW", "HIKS", "PMU"]
+	for building in building_list:
+		_schedule_for_each_building(building)
 
 func _input(event):
 	if event is InputEventMouseMotion or event is InputEventMouseButton:
@@ -177,3 +182,36 @@ func _on_rank_pressed():
 	Attributes.xacademic = $CharacterBody2D.global_position.x
 	Attributes.yacademic = $CharacterBody2D.global_position.y
 	get_tree().change_scene_to_file("res://scenes/rank_page/rank_page.tscn")
+	
+func _schedule_for_each_building(building):
+	var complete_string = ""
+	var count = 0
+	if (Attributes.course_num == 1) :
+		pass
+	else:
+		for i in range (1, Attributes.course_num):
+			var location = "course" + str(i) + "_location"
+			var courseLocation = Attributes.courseLocations[location]
+			if courseLocation == building:
+				count+=1
+				var name = "course" + str(i) + "_name"
+				var courseName = Attributes.courseNames[name]
+				var time = "course" + str(i) + "_time"
+				var courseTime = Attributes.courseTimes[time]
+				var day = "course" + str(i) + "_days"
+				var Days_dict = Attributes.courseDays[day]
+				var courseDays = ""
+				for j in Days_dict:
+					if Days_dict[j] == 1:
+						courseDays += j
+				var format_string = "| Course Name: %s | Time: %s | Days: %s | Location: %s |\n"
+				var actual_string = format_string % [courseName, courseTime, courseDays, courseLocation]
+				complete_string += actual_string
+	if count == 0:
+		find_child(building).visible = false
+	else:
+		find_child(building).visible = true
+		var count_string = "You have %s Courses in this building.\n" + complete_string
+		var string_with_count = count_string % [count]
+		find_child(building).text= string_with_count 
+		
