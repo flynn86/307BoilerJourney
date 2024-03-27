341adf23e9574a00b30f60b3bdc4a7c912069214
ProgrammerCharles890 <112196013+ProgrammerCharles890@users.noreply.github.com>
2024-03-27 12:00:20 -0500

    Update SaveUtils.gd
    Restored deleted edits in SaveUtils.gd.

--- src/SaveUtils.gd
+++ src/SaveUtils.gd
@@ -3,13 +3,14 @@ extends Node
 func save():
 	var file = FileAccess.open(Attributes.filename, FileAccess.WRITE)
 	var save_data: Dictionary = {
-		#"xp" : Attributes.xp,
+		"xp" : Attributes.xp,
 		"username" : Attributes.username,
 		"xhousing" : Attributes.xhousing,
 		"yhousing" : Attributes.yhousing,
 		"xacademic" : Attributes.xacademic,
 		"yacademic" : Attributes.yacademic,
 		"location" : Attributes.location,
+		"rank" : Attributes.rank,
 		
 		# collectables
 		
@@ -51,13 +52,14 @@ func load(savefile: String):
 		var file = FileAccess.open(savefile, FileAccess.READ)
 		var line = JSON.parse_string(file.get_line())
 		if line:
-			#Attributes.xp = line["xp"]
+			Attributes.xp = line["xp"]
 			Attributes.username = line["username"]
 			Attributes.xhousing = line["xhousing"]
 			Attributes.yhousing = line["yhousing"]
 			Attributes.xacademic = line["xacademic"]
 			Attributes.yacademic = line["yacademic"]
 			Attributes.location = line["location"]
+			Attributes.rank = line["rank"]
 			
 			# collectables
 			
