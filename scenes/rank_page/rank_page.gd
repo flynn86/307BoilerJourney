0a822a6695f4785c9d9bae8045a2c2840e2546a7
flynn86 <colinflynn514@gmail.com>
2024-03-28 12:47:35 -0400

    adding logic for rank page

--- scenes/rank_page/rank_page.gd
+++ scenes/rank_page/rank_page.gd
@@ -1,9 +1,17 @@
 extends Control
 
 func _ready():
+	if Attributes.xp <= 29:
+		Attributes.rank = "Freshman"
+	elif Attributes.xp <= 59: 
+		Attributes.rank = "Sophmore"
+	elif Attributes.xp <= 89:
+		Attributes.rank = "Junior"
+	else: 
+		Attributes.rank = "Senior"
 	$pivot/Shield/Label.text = Attributes.rank
-	$Label2.text = "Remaining XP Until Next Rank: " + str(30 - Attributes.xp) + ("XP")
-	$ProgressBar.value = Attributes.xp
+	$Label2.text = "Remaining XP Until Next Rank: " + str(30 - (Attributes.xp % 30)) + ("XP")
+	$ProgressBar.value = (Attributes.xp % 30) / 30
 
 func _on_close_button_pressed():
 	get_tree().change_scene_to_file(Attributes.location)
