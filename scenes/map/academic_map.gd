a773354ba21e9a64db620db3cf50d16101a93ed6
ProgrammerCharles890 <112196013+ProgrammerCharles890@users.noreply.github.com>
2024-03-29 15:58:27 -0500

    Update academic_map.gd
    Removed set_time_state function from script, which was causing error

--- scenes/map/academic_map.gd
+++ scenes/map/academic_map.gd
@@ -19,7 +19,6 @@ func _ready():
 	elif (Attributes.season == "Winter"):
 		get_node("WinterVariantA").visible = true
 	if (Attributes.night):
-		get_node("day_night").set_time_state(1)
 		get_node("CharacterBody2D/Player/player_light").visible = true
 	var building_list = ["ARMS", "PUSH", "HAMP", "PHYS", "RNPH", "JNSN", "FRNY", "DSCB", "MSEE", "CHAS", "BHEE", "AR", "ELLT", 
  					"HOVD", "ME", "WALC", "POTR", "LMBS", "KNOY", "DUDL", "HAAS", "PSYC", "PRCE", "CL50", "MATH", "SC", 
@@ -109,16 +108,6 @@ func _on_change_spring_pressed():
 
 func _on_day_night_button_pressed():
 	Attributes.day_night_enabled = ! Attributes.day_night_enabled
-	#var temp = get_node("day_night")
-	#var temp2 = get_node("CharacterBody2D/Player/player_light")
-	#if (temp.get_time_state() == 0):
-	#	Attributes.night = true;
-	#	temp.set_time_state(1)
-	#	temp2.visible = true
-	#else:
-	#	Attributes.night = false;
-	#	temp.set_time_state(0)
-	#	temp2.visible = false
 
 func _on_menu_button_pressed():
 	Attributes.xacademic = $CharacterBody2D.global_position.x
