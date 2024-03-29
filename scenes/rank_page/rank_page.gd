d6070bc1767245bbd0e5c25c921fc1ad56285914
zandercarpenter <117390566+zandercarpenter@users.noreply.github.com>
2024-03-29 05:53:43 -0400

    Implemented XP leaderboard

--- scenes/rank_page/rank_page.gd
+++ scenes/rank_page/rank_page.gd
@@ -2,13 +2,17 @@ extends Control
 
 func _ready():
 	$pivot/Shield/Label.text = Attributes.rank
+	(Attributes.database).query("SELECT xp FROM Players WHERE username = '" + Attributes.username + "'")
+	var curXP
+	for i in (Attributes.database).query_result:
+		curXP = i["xp"]
 	if (Attributes.rank == "Senior"):
 		$Label2.text = "Remaining XP Until Next Rank: You are the highest rank"
 		$ProgressBar.value = 100
 	else: 
-		$Label2.text = "Remaining XP Until Next Rank: " + str(30 - (Attributes.xp % 30)) + ("XP")
-		$ProgressBar.value = 100 * (Attributes.xp % 30) / 30
-	$Label3.text = "Total XP: " + str(Attributes.xp)
+		$Label2.text = "Remaining XP Until Next Rank: " + str(30 - (curXP % 30)) + ("XP")
+		$ProgressBar.value = 100 * (curXP % 30) / 30
+	$Label3.text = "Total XP: " + str(curXP)
 
 
 func _on_close_button_pressed():
