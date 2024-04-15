a75b6a03797e01a40a706f10d53166c2b1d06e0a
flynn86 <colinflynn514@gmail.com>
2024-04-15 15:46:02 -0400

    continuing work on reviews page

--- scenes/restaurant_page/review_page.gd
+++ scenes/restaurant_page/review_page.gd
@@ -1,23 +1,48 @@
 extends Panel
 
 func _ready():
-	var data : Array = (Attributes.database).select_rows("%s_Review" % Attributes.currentRest, "username != 'null'", ["*"])
-	var data_line : String = ""
-	for i in data.size():
-		data_line += "From " + data[i].username + "on " + data[i].date + ": " + data[i].description + '\n'
-	$Label.text = data_line
+	if (Attributes.currentRest == "XXX"):
+		$Title.text = "Tripple XXX Family Restaurant: Reviews"
+	else: 
+		$Title.text = Attributes.currentRest
+	var data : Array = (Attributes.database).select_rows("%s_review" % Attributes.currentRest, "username != 'null'", ["*"])
+	if (Attributes.database).error_message.contains("no such table:"):
+		var table = {
+		"username" : {"data_type":"text"},
+		"description" : {"data_type":"text"},
+		"date" : {"data_type":"text"},
+		"rating" : {"data_type":"text"}
+		}
+		(Attributes.database).create_table("%s_review" % Attributes.currentRest, table)
+		(Attributes.database).drop_table("%s_review" % Attributes.currentRest)
+		(Attributes.database).create_table("%s_review" % Attributes.currentRest, table)
+		SaveUtils.save()
+	else: 
+		var data_line : String = ""
+		for i in data.size():
+			data_line += "Review From: " + data[i].username + " on " + data[i].date + '\n' + "Description: " + data[i].description + '\n' + "Rating: " + data[i].rating + " Stars" + '\n' + '\n'
+		$Reviews.text = data_line
 	
 func _process(_delta):
 	_ready()
 
 func _leave_review_pressed():
 	var timedate : Dictionary = Time.get_datetime_dict_from_system()
+	var given_r = $LineEdit2.text
+	var number = float(given_r)
+	var final_val
+	if str(number) == given_r and number >= 0 and number <= 5:
+		final_val = number
+	else:
+		final_val = 3
 	var data = {
 		"username" = Attributes.username,
 		"description" = $LineEdit.text,
-		"date" = str(timedate.month) + "-" + str(timedate.day) + "-" + str(timedate.year),
+		"date" = str(timedate.month) + "/" + str(timedate.day) + "/" + str(timedate.year),
+		"rating" = str(final_val)
 	}
-	(Attributes.database).insert_row("%s_Chat" % Attributes.serverName, data)
+	print(Attributes.currentRest)
+	(Attributes.database).insert_row("%s_review" % Attributes.currentRest, data)
 
 func _on_back_to_menu_pressed():
 	get_tree().change_scene_to_file("res://scenes/restaurant_page/restaurant_page.tscn")
