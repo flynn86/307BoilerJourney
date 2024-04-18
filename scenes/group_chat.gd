91e155eff5ece3183106cfe26cd8f3496222319e
KashviS <112533730+KashviS@users.noreply.github.com>
2024-04-18 19:29:54 -0400

    modifies code of group chat

--- scenes/group_chat.gd
+++ scenes/group_chat.gd
@@ -15,7 +15,7 @@ func _ready():
 	var data_line : String = ""
 	for i in data.size():
 		data_line += "From " + data[i].username + " at " + data[i].time + " on " + data[i].date + ": " + data[i].message + '\n'
-	$Label.text = data_line
+	$Panel/Label.text = data_line
 
 func _process(_delta):
 	await get_tree().create_timer(1.0).timeout
@@ -28,7 +28,7 @@ func _on_send_message_pressed():
 	var timedate : Dictionary = Time.get_datetime_dict_from_system()
 	var data = {
 		"username" = Attributes.username,
-		"message" = $LineEdit.text,
+		"message" = $Panel/LineEdit.text,
 		"date" = str(timedate.month) + "-" + str(timedate.day) + "-" + str(timedate.year),
 		"time" = "%02d:%02d:%02d" % [timedate.hour, timedate.minute, timedate.second]
 	}
